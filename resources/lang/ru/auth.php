5db52004a942d68126c49f3f8c508a681eee3611
Никита Фисун <nikita_c47@outlook.com>
2020-03-09 14:03:00 +0500

    Русификация сообщений - Авторизация; - Пагинация; - Сбросы пароля.

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Указанные учетные данные отсутствуют в нашей базе.',
+    'throttle' => 'Слишком много попыток входа. Пожалуйста, попробуйте еще раз через указанное количество секунд: :seconds.',
 
 ];
